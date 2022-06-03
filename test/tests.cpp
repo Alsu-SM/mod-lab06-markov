52d0cccfda2ffd3d351c2e9b44af8492c89a6c85
Alsu-SM <70974998+Alsu-SM@users.noreply.github.com>
2022-06-03 23:42:52 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,2 +1,12 @@
 // Copyright 2021 GHA Test Team
 #include <gtest/gtest.h>
+#include <sstream>
+#include <string>
+#include "textgen.h"
+
+TEST(task1, test1) {
+    TextGenerator tg = TextGenerator("Text.txt", 2);
+    std::string text = tg.CreateText(1000);
+    TextGenerator::prefix temp = tg.GetPref(0);
+    ASSERT_EQ(temp.size(), 2);
+}

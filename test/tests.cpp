9c16b2fc4a9aed88100595e9ecadad16f3bb90ba
Alsu-SM <70974998+Alsu-SM@users.noreply.github.com>
2022-06-04 00:04:00 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -2,7 +2,7 @@
 #include <gtest/gtest.h>
 #include <sstream>
 #include <string>
-#include "generateText.h"
+#include "textgen.h"
 
 TEST(task1, test1) {
     TextGenerator tg = TextGenerator("Text.txt", 2);

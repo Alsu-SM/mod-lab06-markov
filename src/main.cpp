a60d65085f8eec053a24087d17280828affb1666
Alsu-SM <70974998+Alsu-SM@users.noreply.github.com>
2022-06-04 00:00:56 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 // Copyright 2021 GHA Test Team
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <vector>
+#include <deque>
+#include <map>
 #include "textgen.h"
 
 int main() {
-    TextGenerator t = TextGenerator("Text.txt", 2);
+    TextGen t = TextGen("Text1.txt", 2);
     std::string text = t.CreateText(1000);
     std::cout << text;
 }
